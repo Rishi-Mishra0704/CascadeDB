1b9e1e87050d3c11ef752016fa2876a7f0ab1093
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-03-11 00:35:05 +0530

    add accept tcp connection

---
+++ p2p/tcp_transport.go
@@ -0,0 +1,50 @@
+package p2p
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"sync"
+)
+
+type TcpTransport struct {
+	listenAddress string
+	listener      net.Listener
+	mu            sync.RWMutex
+	peers         map[net.Addr]Peer
+}
+
+func NewTCPTransport(listenAddr string) *TcpTransport {
+	return &TcpTransport{
+		listenAddress: listenAddr,
+	}
+}
+
+func (t *TcpTransport) listenAndAccept() error {
+
+	var err error
+	t.listener, err = net.Listen("tcp", t.listenAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go t.startAcceptLoop()
+
+	return nil
+}
+
+func (t *TcpTransport) startAcceptLoop() {
+	for {
+		conn, err := t.listener.Accept()
+		if err != nil {
+			fmt.Printf("TCP accept error %s\n", err)
+		}
+
+		go t.handleConn(conn)
+
+	}
+}
+
+func (t *TcpTransport) handleConn(conn net.Conn) {
+	fmt.Printf("New Incoming Connection %+v\n", conn)
+}
---
+++ p2p/transport.go
@@ -0,0 +1,13 @@
+package p2p
+
+// Peer is an interface that represents remote node.
+type Peer interface {
+}
+
+/*
+Trasnport is anything that controls the communication
+between the nodes in the network
+forms: tcp udp websockets ..etc
+*/
+type Transport interface {
+}
