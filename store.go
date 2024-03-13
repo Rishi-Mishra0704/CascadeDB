6ecab0bcd302aa06fee3ec2fe76b21716906f463
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-03-13 13:01:10 +0530

    add CAS system

--- store.go
+++ store.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"bytes"
+	"crypto/md5"
+	"crypto/sha1"
+	"encoding/hex"
 	"io"
 	"log"
 	"os"
+	"strings"
 )
+
+func CasPathTransformFunc(key string) string {
+	hash := sha1.Sum([]byte(key))
+	hashStr := hex.EncodeToString(hash[:])
+
+	blockSize := 5
+	sliceLen := len(hashStr) / blockSize
+
+	paths := make([]string, sliceLen)
+	for i := 0; i < sliceLen; i++ {
+		from, to := i*blockSize, (i*blockSize)+blockSize
+
+		paths[i] = hashStr[from:to]
+
+	}
+	return strings.Join(paths, "/")
+}
 
 type PathTransformFunc func(string) string
 
@@ -32,7 +54,13 @@ func (s *Store) WriteStream(key string, r io.Reader) error {
 		return err
 	}
 
-	filename := "somefilename"
+	buf := new(bytes.Buffer)
+
+	io.Copy(buf, r)
+
+	filenameBytes := md5.Sum(buf.Bytes())
+
+	filename := hex.EncodeToString(filenameBytes[:])
 
 	pathAndFileName := pathname + "/" + filename
 
@@ -40,7 +68,7 @@ func (s *Store) WriteStream(key string, r io.Reader) error {
 	if err != nil {
 		return err
 	}
-	n, err := io.Copy(f, r)
+	n, err := io.Copy(f, buf)
 	if err != nil {
 		return err
 	}
--- store_test.go
+++ store_test.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"testing"
 )
 
+func TestPathTransformFunc(t *testing.T) {
+	key := "mombestpicture"
+	pathname := CasPathTransformFunc(key)
+	fmt.Println(pathname)
+	expectedPath := "cf5d4/b01c4/d9438/c22c5/6c832/f83bd/3e8c6/304f9"
+	if pathname != expectedPath {
+		t.Errorf("have %s want %s", pathname, expectedPath)
+	}
+}
+
 func TestStore(t *testing.T) {
 	opts := StoreOpts{
-		PathTransformFunc: DefaultTransformFunc,
+		PathTransformFunc: CasPathTransformFunc,
 	}
 	s := NewStore(opts)
 
