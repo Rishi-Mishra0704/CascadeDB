93c469f81c91c34c47781eb788f2e16227718dd1
Rishi Mishra <114818980+Rishi-Mishra0704@users.noreply.github.com>
2024-05-31 00:35:44 +0530

    Update handshake.go

--- p2p/handshake.go
+++ p2p/handshake.go
@@ -1,14 +1,6 @@
 package p2p
 
-import "errors"
+// HandshakeFunc... ?
+type HandshakeFunc func(Peer) error
 
-/*
-	ErrInvalidHandshakeError is returned when a connection
-	between local and remote nodes is not established
-*/
-var ErrInvalidHandshakeError = errors.New("invalid handshake")
-
-// HandShakeFunc is a function that is called when a new connection is established
-type HandShakeFunc func(Peer) error
-
-func NOPHandShakeFunc(Peer) error { return nil }
+func NOPHandshakeFunc(Peer) error { return nil }
