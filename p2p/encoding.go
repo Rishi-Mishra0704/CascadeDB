962007d1ffa04b88cdb166a256896f503b694b74
Rishi Mishra <114818980+Rishi-Mishra0704@users.noreply.github.com>
2024-05-31 00:35:13 +0530

    Update encoding.go

--- p2p/encoding.go
+++ p2p/encoding.go
@@ -9,23 +9,35 @@ type Decoder interface {
 	Decode(io.Reader, *RPC) error
 }
 
-type GobDecoder struct {
+type GOBDecoder struct{}
+
+func (dec GOBDecoder) Decode(r io.Reader, msg *RPC) error {
+	return gob.NewDecoder(r).Decode(msg)
 }
 
-func (doc GobDecoder) Decode(r io.Reader, rpc *RPC) error {
-	return gob.NewDecoder(r).Decode(rpc)
-}
+type DefaultDecoder struct{}
 
-type DefaultDecoder struct {
-}
+func (dec DefaultDecoder) Decode(r io.Reader, msg *RPC) error {
+	peekBuf := make([]byte, 1)
+	if _, err := r.Read(peekBuf); err != nil {
+		return nil
+	}
 
-func (doc DefaultDecoder) Decode(r io.Reader, rpc *RPC) error {
+	// In case of a stream we are not decoding what is being sent over the network.
+	// We are just setting Stream true so we can handle that in our logic.
+	stream := peekBuf[0] == IncomingStream
+	if stream {
+		msg.Stream = true
+		return nil
+	}
+
 	buf := make([]byte, 1028)
 	n, err := r.Read(buf)
 	if err != nil {
 		return err
 	}
-	rpc.Payload = buf[:n]
-	return nil
 
+	msg.Payload = buf[:n]
+
+	return nil
 }
