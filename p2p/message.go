7aad4ec88d4c1beb6d80dad9a3002b07b393526f
Rishi Mishra <114818980+Rishi-Mishra0704@users.noreply.github.com>
2024-05-31 00:36:45 +0530

    Update message.go

--- p2p/message.go
+++ p2p/message.go
@@ -1,13 +1,14 @@
 package p2p
 
-/*
+const (
+	IncomingMessage = 0x1
+	IncomingStream  = 0x2
+)
 
-Message hold any arbitrary data that is being sent over each transport
-between two nodes in a network
-
-
-*/
+// RPC holds any arbitrary data that is being sent over the
+// each transport between two nodes in the network.
 type RPC struct {
 	From    string
 	Payload []byte
+	Stream  bool
 }
